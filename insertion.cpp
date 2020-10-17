36fa01cfe75018c4043a9bf4114fab8bcafe68ca
wwjdtm <jje0ng@kookmin.ac.kr>
2020-10-17 20:25:52 +0900

    Add files via upload
    insertionsort

---
+++ insertion.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <climits>
+using namespace std;
+
+int main(void) {
+    int n;
+    vector<int> v;
+
+    cin >> n;
+
+    for (int i=0; i<n; i++){
+        int d;
+        cin >> d ;
+        v.push_back(d);
+    }
+
+    // insertion sort
+     for (int j=1; j<v.size(); j++){
+
+        int key = v[j];
+        int i = j-1;
+        while (i>=0 && v[i]>key){
+            v[i+1] = v[i];
+            i--;
+        }
+        v[i+1] = key;
+
+    }
+
+    for (int i=0; i<n; i++){
+        cout << v[i] << endl;
+    }
+
+    return 0;
+}
